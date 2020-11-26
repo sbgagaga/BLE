6969ac91e77e77b90097feb960e8c671c4c8dddf
sbgagaga <464786640@qq.com>
2020-11-26 11:24:09 +0800

    初步搞好了蓝牙名字的修改

---
+++ obj/flash.d
@@ -0,0 +1,6 @@
+.\obj\flash.o: ..\..\sdk\plactform\driver\flash\flash.c
+.\obj\flash.o: ..\..\sdk\plactform\driver\flash\flash.h
+.\obj\flash.o: D:\LoadDown\Kile\ARM\ARMCC\Bin\..\include\stdint.h
+.\obj\flash.o: ..\..\sdk\plactform\arch\BK3432_reg.h
+.\obj\flash.o: D:\LoadDown\Kile\ARM\ARMCC\Bin\..\include\string.h
+.\obj\flash.o: ..\..\sdk\plactform\core_modules\rf\api\rf.h
